4ce119f58a13ebc0441d2c0024b5aa11bc44f1db
abdalazzezzalsalahat <abdalazzezzalsalahat@icloud.com>
2021-02-22 14:52:23 +0200

    new new code in js, css, html files

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const imgPaths = ['bag.jpg'
                 ,'banana.jpg'
                 ,'bathroom.jpg'
@@ -24,8 +23,7 @@ const imgPaths = ['bag.jpg'
 const imgs = [];
 const extention = [];
 let cuntr = 0;
-const laps = 25;
-
+const laps = 5;
 const imgSection = document.getElementById('imgSec');
 const ritImg = document.getElementById('rit');
 const midImg = document.getElementById('mid');
@@ -64,19 +62,36 @@ function render () {
     lftImg.src = randImg.all[lftIndx].path;
     lftImg.title = randImg.all[lftIndx].name;
     lftImg.alt = randImg.all[lftIndx].name;
+    let currntLftIndx = lftIndx;
+    if(lftIndx !== currntLftIndx){
+      Math.abs(lftIndx -= imgs.length);
+      currntLftIndx = lftIndx;
+    }
     randImg.all[lftIndx].views++;
     let midIndex = lftIndx;
+    let currntMid = midIndex;
+    if (midIndex !== currntMid){
+      Math.abs(midIndex -= imgs.length);
+      currntMid = midIndex;
+    }
+    
     while (midIndex === lftIndx) {
       midIndex = randmVal(lftIndx, imgs.length-1);
       if(midIndex !== lftIndx) {
         break;
       }
     }
+    debugger;
     midImg.src = randImg.all[midIndex].path;
     midImg.title = randImg.all[midIndex].name;
     midImg.alt = randImg.all[midIndex].name;
     randImg.all[midIndex].views++;
     let rightIndex;
+    let currntRightIndx = rightIndex;
+    if (rightIndex !== currntRightIndx){
+      Math.abs(rightIndex -= imgs.length);
+      currntRightIndx = rightIndex;
+    }
     while (midIndex !== lftIndx) {
       rightIndex = randmVal(0, imgs.length -1);
       if(rightIndex === lftIndx || rightIndex === midIndex){
@@ -90,6 +105,41 @@ function render () {
     ritImg.alt = randImg.all[rightIndex].name;
     randImg.all[rightIndex].views++;
 }
+function barChart (){
+  const ctx = document.getElementById('chartCont').getContext('2d');
+  const liked = [];
+  const viewed = [];
+  for (let i = 0; i < imgs.length; i++) {
+    liked.push(randImg.all[i].likes);
+    viewed.push(randImg.all[i].views);
+  }
+  new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: imgs,
+      datasets: [
+        {
+          barPercentage: 0.5,
+          // barThickness: 6,
+          borderWidth: 1,
+          label: '# of Likes:',
+          backgroundColor: '#2e2e2e',
+          borderColor: '#2e2e2e',
+          data: liked,
+        },
+        {
+          barPercentage: 0.5,
+          borderWidth: 1,
+          label: '# of Views:',
+          backgroundColor: '#e2e2e2',
+          borderColor: '#e2e2e2',
+          data: viewed,
+        },
+      ],
+    },
+    options: {indexAxis: 'y'}
+  })
+}
 function clkabilty(event) {
     if(event.target.id !== 'imgSec'){
       for (let i = 0; i < randImg.all.length; i++) {
@@ -101,6 +151,7 @@ function clkabilty(event) {
       cuntr = cuntr + 1;
       if(cuntr === laps){
         imgSection.removeEventListener('click', clkabilty);
+        barChart();
         const resBtn = document.createElement('button');
         resultsBtn.appendChild(resBtn);
         resBtn.textContent = 'View Results';
@@ -122,4 +173,3 @@ function resultBtn(event) {
 }
 render();
 imgSec.addEventListener('click', clkabilty);
-
